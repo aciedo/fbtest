6af3510df14961932160e061a933a16535c7c3e2
Azz <asteria@valera.co>
2023-01-15 11:57:47 +0000

    it works properly now

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 "use strict";
 exports.__esModule = true;
 var flatbuffers = require("flatbuffers");
-var reps = 1000000;
+var reps = 2500000;
 var monster_generated_1 = require("./monster_generated");
-for (var i = 0; i < reps; i++) {
-    doFb();
-}
-doFb(true);
-doJson(true);
 // Create a `flatbuffer.Builder`, which will be used to create our
 // monsters' FlatBuffers.
 var builder = new flatbuffers.Builder(1024);
@@ -55,91 +50,98 @@ monster_generated_1.Monster.addEquipped(builder, axe); // Union data
 builder.finish(orc); // You could also call `MyGame.Monster.finishMonsterBuffer(builder, orc);`.
 // This must be called after `finish()`.
 var fbBuf = builder.asUint8Array(); // Of type `Uint8Array`.
-var start = performance.now();
-for (var i = 0; i < reps; i++) {
+var jsonStr = "{\"pos\":{\"x\":1,\"y\":2,\"z\":3},\"hp\":300,\"name\":\"Orc\",\"inventory\":[0,1,2,3,4,5,6,7,8,9],\"color\":\"Red\",\"weapons\":[{\"name\":\"Sword\",\"damage\":3},{\"name\":\"Axe\",\"damage\":5}],\"equipped_type\":\"Weapon\",\"equipped\":{\"name\":\"Axe\",\"damage\":5},\"path\":[{\"x\":4,\"y\":5,\"z\":6},{\"x\":1,\"y\":2,\"z\":3}]}";
+console.log("Warming up...");
+for (var i = 0; i < 250000; i++) {
     doFb();
+    doJson();
 }
-var end = performance.now();
-console.log("Time: ".concat(end - start, " ms"));
-var fbOpUs = (end - start) / reps * 1000;
-console.log("us per op: ".concat(fbOpUs, " us"));
-start = performance.now();
+var ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+function to_b58(buf) {
+    var str = "";
+    var carry = 0;
+    var i = 0;
+    while (i < buf.length || carry) {
+        var acc = carry;
+        var j = 0;
+        while (j < buf.length) {
+            acc = acc * 256 + buf[j];
+            buf[j] = Math.floor(acc / 58);
+            acc %= 58;
+            j++;
+        }
+        str += ALPHABET[acc];
+        carry = buf[i] === 0 ? 1 : 0;
+        i++;
+    }
+    return str;
+}
+var fbOut = doFb();
+console.log("\n--------- FlatBuffer:");
+console.log("Encoded (b58): ".concat(to_b58(fbOut)));
+console.log("Length: ".concat(fbOut.length, " bytes"));
+var start = performance.now();
 for (var i = 0; i < reps; i++) {
     doJson();
 }
-end = performance.now();
-console.log("Time: ".concat(end - start, " ms"));
+var end = performance.now();
 var jsonOpUs = (end - start) / reps * 1000;
 console.log("us per op: ".concat(jsonOpUs, " us"));
-console.log("fbOpUs / jsonOpUs: ".concat(fbOpUs / jsonOpUs));
-function doFb(output) {
-    if (output === void 0) { output = false; }
+var jsonOut = doJson();
+console.log("\n--------- JSON:");
+console.log("Encoded: ".concat(jsonOut));
+console.log("Length: ".concat(jsonOut.length, " bytes"));
+start = performance.now();
+for (var i = 0; i < reps; i++) {
+    doFb();
+}
+end = performance.now();
+var fbOpUs = (end - start) / reps * 1000;
+console.log("us per op: ".concat(Math.round(fbOpUs * 1000) / 1000, " us"));
+console.log("fbOpUs / jsonOpUs: ".concat(Math.round((fbOpUs / jsonOpUs) * 1000) / 1000, "x"));
+console.log("Size improvement: ".concat(Math.round((fbOut.length / jsonOut.length) * 1000) / 1000, "x"));
+function doFb() {
+    var _a, _b, _c, _d, _e, _f, _g;
+    // extract the monster from bytes
     var m = new flatbuffers.ByteBuffer(fbBuf);
     var monster = monster_generated_1.Monster.getRootAsMonster(m);
-    // Create a `flatbuffer.Builder`, which will be used to create our
-    // monsters' FlatBuffers.
+    // clone the monster
     var builder = new flatbuffers.Builder(1024);
-    var weaponOne = builder.createString('Sword');
-    var weaponTwo = builder.createString('Axe');
-    // Create the first `Weapon` ('Sword').
+    var weaponOne = builder.createString((_a = monster.weapons(0)) === null || _a === void 0 ? void 0 : _a.name());
+    var weaponTwo = builder.createString((_b = monster.weapons(1)) === null || _b === void 0 ? void 0 : _b.name());
     monster_generated_1.Weapon.startWeapon(builder);
     monster_generated_1.Weapon.addName(builder, weaponOne);
-    monster_generated_1.Weapon.addDamage(builder, 3);
+    monster_generated_1.Weapon.addDamage(builder, (_c = monster.weapons(0)) === null || _c === void 0 ? void 0 : _c.damage());
     var sword = monster_generated_1.Weapon.endWeapon(builder);
-    // Create the second `Weapon` ('Axe').
     monster_generated_1.Weapon.startWeapon(builder);
     monster_generated_1.Weapon.addName(builder, weaponTwo);
-    monster_generated_1.Weapon.addDamage(builder, 5);
+    monster_generated_1.Weapon.addDamage(builder, (_d = monster.weapons(1)) === null || _d === void 0 ? void 0 : _d.damage());
     var axe = monster_generated_1.Weapon.endWeapon(builder);
-    // Serialize a name for our monster, called 'Orc'.
-    var name = builder.createString('Orc');
-    // Create a `vector` representing the inventory of the Orc. Each number
-    // could correspond to an item that can be claimed after he is slain.
-    var treasure = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    var inv = monster_generated_1.Monster.createInventoryVector(builder, treasure);
-    // Create an array from the two `Weapon`s and pass it to the
-    // `createWeaponsVector()` method to create a FlatBuffer vector.
-    var weaps = [sword, axe];
-    var weapons = monster_generated_1.Monster.createWeaponsVector(builder, weaps);
+    var weapons = monster_generated_1.Monster.createWeaponsVector(builder, [sword, axe]);
+    var name = builder.createString(monster.name());
+    var inv = monster_generated_1.Monster.createInventoryVector(builder, monster.inventoryArray());
     monster_generated_1.Monster.startPathVector(builder, 2);
     monster_generated_1.Vec3.createVec3(builder, 1.0, 2.0, 3.0);
     monster_generated_1.Vec3.createVec3(builder, 4.0, 5.0, 6.0);
     var path = builder.endVector();
-    // Create our monster by using `startMonster()` and `endMonster()`.
     monster_generated_1.Monster.startMonster(builder);
-    monster_generated_1.Monster.addPos(builder, monster_generated_1.Vec3.createVec3(builder, 1.0, 2.0, 3.0));
-    monster_generated_1.Monster.addHp(builder, 300);
-    monster_generated_1.Monster.addColor(builder, monster_generated_1.Color.Red);
+    monster_generated_1.Monster.addPos(builder, monster_generated_1.Vec3.createVec3(builder, (_e = monster.pos()) === null || _e === void 0 ? void 0 : _e.x(), (_f = monster.pos()) === null || _f === void 0 ? void 0 : _f.y(), (_g = monster.pos()) === null || _g === void 0 ? void 0 : _g.z()));
+    monster_generated_1.Monster.addHp(builder, monster.hp());
+    monster_generated_1.Monster.addColor(builder, monster.color());
     monster_generated_1.Monster.addName(builder, name);
     monster_generated_1.Monster.addInventory(builder, inv);
     monster_generated_1.Monster.addWeapons(builder, weapons);
-    monster_generated_1.Monster.addEquippedType(builder, monster_generated_1.Equipment.Weapon);
+    monster_generated_1.Monster.addEquippedType(builder, monster.equippedType());
     monster_generated_1.Monster.addEquipped(builder, axe);
     monster_generated_1.Monster.addPath(builder, path);
     var orc = monster_generated_1.Monster.endMonster(builder);
-    monster_generated_1.Monster.addEquippedType(builder, monster_generated_1.Equipment.Weapon); // Union type
-    monster_generated_1.Monster.addEquipped(builder, axe); // Union data
-    // Call `finish()` to instruct the builder that this monster is complete.
-    builder.finish(orc); // You could also call `MyGame.Monster.finishMonsterBuffer(builder, orc);`.
-    // This must be called after `finish()`.
-    var buf = builder.asUint8Array(); // Of type `Uint8Array`.
-    if (output) {
-        // convert buf to hex string
-        var hex = '';
-        for (var i = 0; i < buf.length; i++) {
-            hex += ('00' + buf[i].toString(16)).slice(-2);
-        }
-        console.log(buf);
-        console.log(hex);
-        console.log(buf.length);
-    }
+    builder.finish(orc);
+    return builder.asUint8Array();
 }
-function doJson(output) {
-    if (output === void 0) { output = false; }
-    var monster = JSON.parse("{\"pos\":{\"x\":1,\"y\":2,\"z\":3},\"hp\":300,\"name\":\"Orc\",\"inventory\":[0,1,2,3,4,5,6,7,8,9],\"color\":\"Red\",\"weapons\":[{\"name\":\"Sword\",\"damage\":3},{\"name\":\"Axe\",\"damage\":5}],\"equipped_type\":\"Weapon\",\"equipped\":{\"name\":\"Axe\",\"damage\":5},\"path\":[{\"x\":4,\"y\":5,\"z\":6},{\"x\":1,\"y\":2,\"z\":3}]}");
+function doJson() {
+    // extract the monster from bytes
+    var monster = JSON.parse(jsonStr);
+    // clone the monster
     var s = JSON.stringify(monster);
-    if (output) {
-        console.log(s);
-        console.log(s.length);
-    }
+    return s;
 }
--- index.ts
+++ index.ts
@@ -1,15 +1,8 @@
 import * as flatbuffers from 'flatbuffers';
 
-const reps = 1000000;
+const reps = 2_500_000;
 
 import { Weapon, Monster, Vec3, Color, Equipment } from './monster_generated';
-
-for (let i = 0; i < reps; i++) {
-    doFb();
-}
-
-doFb(true);
-doJson(true);
 
 // Create a `flatbuffer.Builder`, which will be used to create our
 // monsters' FlatBuffers.
@@ -68,112 +61,122 @@ builder.finish(orc); // You could also call `MyGame.Monster.finishMonsterBuffer(
 
 // This must be called after `finish()`.
 let fbBuf = builder.asUint8Array(); // Of type `Uint8Array`.
+let jsonStr = `{"pos":{"x":1,"y":2,"z":3},"hp":300,"name":"Orc","inventory":[0,1,2,3,4,5,6,7,8,9],"color":"Red","weapons":[{"name":"Sword","damage":3},{"name":"Axe","damage":5}],"equipped_type":"Weapon","equipped":{"name":"Axe","damage":5},"path":[{"x":4,"y":5,"z":6},{"x":1,"y":2,"z":3}]}`
+
+console.log("Warming up...")
+for (let i = 0; i < 250_000; i++) {
+    doFb();
+    doJson();
+}
+
+const ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+function to_b58(buf: Uint8Array) {
+  let str = "";
+  let carry = 0;
+  let i = 0;
+  while (i < buf.length || carry) {
+    let acc = carry;
+    let j = 0;
+    while (j < buf.length) {
+      acc = acc * 256 + buf[j];
+      buf[j] = Math.floor(acc / 58);
+      acc %= 58;
+      j++;
+    }
+    str += ALPHABET[acc];
+    carry = buf[i] === 0 ? 1 : 0;
+    i++;
+  }
+  return str;
+}
+
+const fbOut = doFb();
+console.log(`\n--------- FlatBuffer:`)
+console.log(`Encoded (b58): ${to_b58(fbOut)}`);
+console.log(`Length: ${fbOut.length} bytes`);
 
 let start = performance.now();
+for (let i = 0; i < reps; i++) {
+    doJson();
+}
+let end = performance.now();
+const jsonOpUs = (end - start) / reps * 1000;
+console.log(`us per op: ${jsonOpUs} us`);
+
+const jsonOut = doJson();
+console.log(`\n--------- JSON:`)
+console.log(`Encoded: ${jsonOut}`);
+console.log(`Length: ${jsonOut.length} bytes`);
+
+start = performance.now();
 
 for (let i = 0; i < reps; i++) {
     doFb();
 }
 
-let end = performance.now();
-console.log(`Time: ${end - start} ms`);
-const fbOpUs = (end - start) / reps * 1000;
-console.log(`us per op: ${fbOpUs} us`);
-
-start = performance.now();
-for (let i = 0; i < reps; i++) {
-    doJson();
-}
 end = performance.now();
-console.log(`Time: ${end - start} ms`);
-const jsonOpUs = (end - start) / reps * 1000;
-console.log(`us per op: ${jsonOpUs} us`);
-
-console.log(`fbOpUs / jsonOpUs: ${fbOpUs / jsonOpUs}`);
+const fbOpUs = (end - start) / reps * 1000;
+console.log(`us per op: ${Math.round(fbOpUs * 1000) / 1000} us`);
 
 
+console.log(`fbOpUs / jsonOpUs: ${Math.round((fbOpUs / jsonOpUs) * 1000) / 1000}x`);
+console.log(`Size improvement: ${Math.round((fbOut.length / jsonOut.length) * 1000) / 1000}x`);
 
-function doFb(output = false) {
+
+// yes, flatbuffers are more code to write than JSON, but this code runs 1.6x faster (even though it's pure JS) and is 70% of the size
+function doFb() {
+    // extract the monster from bytes
     let m = new flatbuffers.ByteBuffer(fbBuf)
     let monster = Monster.getRootAsMonster(m);
 
-    // Create a `flatbuffer.Builder`, which will be used to create our
-    // monsters' FlatBuffers.
+    // clone the monster
     let builder = new flatbuffers.Builder(1024);
-    let weaponOne = builder.createString('Sword');
-    let weaponTwo = builder.createString('Axe');
+    let weaponOne = builder.createString(monster.weapons(0)?.name()!);
+    let weaponTwo = builder.createString(monster.weapons(1)?.name()!);
 
-    // Create the first `Weapon` ('Sword').
     Weapon.startWeapon(builder);
     Weapon.addName(builder, weaponOne);
-    Weapon.addDamage(builder, 3);
+    Weapon.addDamage(builder, monster.weapons(0)?.damage()!);
     let sword = Weapon.endWeapon(builder);
 
-    // Create the second `Weapon` ('Axe').
     Weapon.startWeapon(builder);
     Weapon.addName(builder, weaponTwo);
-    Weapon.addDamage(builder, 5);
+    Weapon.addDamage(builder, monster.weapons(1)?.damage()!);
     let axe = Weapon.endWeapon(builder);
 
-    // Serialize a name for our monster, called 'Orc'.
-    let name = builder.createString('Orc');
+    let weapons = Monster.createWeaponsVector(builder, [sword, axe])
 
-    // Create a `vector` representing the inventory of the Orc. Each number
-    // could correspond to an item that can be claimed after he is slain.
-    let treasure = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let inv = Monster.createInventoryVector(builder, treasure);
-
-    // Create an array from the two `Weapon`s and pass it to the
-    // `createWeaponsVector()` method to create a FlatBuffer vector.
-    let weaps = [sword, axe];
-    let weapons = Monster.createWeaponsVector(builder, weaps);
+    let name = builder.createString(monster.name()!);
+    let inv = Monster.createInventoryVector(builder, monster.inventoryArray()!);
 
     Monster.startPathVector(builder, 2);
     Vec3.createVec3(builder, 1.0, 2.0, 3.0);
     Vec3.createVec3(builder, 4.0, 5.0, 6.0);
     let path = builder.endVector();
 
-    // Create our monster by using `startMonster()` and `endMonster()`.
     Monster.startMonster(builder);
-    Monster.addPos(builder, Vec3.createVec3(builder, 1.0, 2.0, 3.0));
-    Monster.addHp(builder, 300);
-    Monster.addColor(builder, Color.Red)
+    Monster.addPos(builder, Vec3.createVec3(builder, monster.pos()?.x()!, monster.pos()?.y()!, monster.pos()?.z()!));
+    Monster.addHp(builder, monster.hp());
+    Monster.addColor(builder, monster.color())
     Monster.addName(builder, name);
     Monster.addInventory(builder, inv);
     Monster.addWeapons(builder, weapons);
-    Monster.addEquippedType(builder, Equipment.Weapon);
+    Monster.addEquippedType(builder, monster.equippedType());
     Monster.addEquipped(builder, axe);
     Monster.addPath(builder, path);
     let orc = Monster.endMonster(builder);
+    builder.finish(orc); 
 
-    Monster.addEquippedType(builder, Equipment.Weapon); // Union type
-    Monster.addEquipped(builder, axe); // Union data
-
-    // Call `finish()` to instruct the builder that this monster is complete.
-    builder.finish(orc); // You could also call `MyGame.Monster.finishMonsterBuffer(builder, orc);`.
-
-    // This must be called after `finish()`.
-    let buf = builder.asUint8Array(); // Of type `Uint8Array`.
-
-    if (output) {
-        // convert buf to hex string
-        let hex = '';
-        for (let i = 0; i < buf.length; i++) {
-            hex += ('00' + buf[i].toString(16)).slice(-2);
-        }
-        console.log(buf)
-        console.log(hex);
-        console.log(buf.length)
-
-    }
+    return builder.asUint8Array();
 }
 
-function doJson(output = false) {
-    const monster = JSON.parse(`{"pos":{"x":1,"y":2,"z":3},"hp":300,"name":"Orc","inventory":[0,1,2,3,4,5,6,7,8,9],"color":"Red","weapons":[{"name":"Sword","damage":3},{"name":"Axe","damage":5}],"equipped_type":"Weapon","equipped":{"name":"Axe","damage":5},"path":[{"x":4,"y":5,"z":6},{"x":1,"y":2,"z":3}]}`);
+// JSON is slower even if it's written in C++, but has less code to write
+function doJson() {
+    // extract the monster from bytes
+    const monster = JSON.parse(jsonStr);
+    // clone the monster
     const s = JSON.stringify(monster);
 
-    if (output) {
-        console.log(s);
-        console.log(s.length);
-    }
+    return s;
 }
